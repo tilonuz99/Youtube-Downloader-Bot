7ccdba67eb75f70b4e1e90c234cbc223768836ad
tilonuz99 <tilonuz@gmail.com>
2021-10-02 07:34:07 +0000

    htt

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,23 +1,26 @@
 from datetime import datetime, timedelta
-from pyrogram import Client, Filters, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram import Message
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
 from bot import user_time
 from config import youtube_next_fetch
-from helper.ytdlfunc import extractYt, create_audio_button, create_video_button, video_audio_button
+from helper.ytdlfunc import extractYt, map_video_button
 import wget
 import os
 from PIL import Image
+from utils.util import humanbytes
 
 ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
 
 
-@Client.on_message(Filters.regex(ytregex))
+@Client.on_message(filters.regex(ytregex))
 async def ytdl(_, message: Message):
     userLastDownloadTime = user_time.get(message.chat.id)
     try:
         if userLastDownloadTime > datetime.now():
             wait_time = round((userLastDownloadTime - datetime.now()).total_seconds())
-            await message.reply_text(f"`Wait {wait_time} seconds before next Request`")
+            await message.reply_text(f"`{wait_time} soniyadan so'ng qayta urining!`")
             return
     except:
         pass
@@ -25,54 +28,28 @@ async def ytdl(_, message: Message):
     user_time[message.chat.id] = datetime.now() + timedelta(seconds=30)
     
     url = message.text.strip()
-    keyboard, thumbnail, title = video_audio_button(url)
-    # buttons = InlineKeyboardMarkup(list(create_audio_button(audioList)))
+    title, thumbnail, videolist = extractYt(url)
+    file_sizes = ""
+    for video in videolist.values():
+        file_size = video['filesize']
+        if file_size > 2147483648:
+            file_sizes += f"🛑 {video['format_note']}:  {humanbytes(file_size)}\n"
+        else:
+            file_sizes += f"✅ {video['format_note']}:  {humanbytes(file_size)}\n"
+    keyboard = map_video_button(videolist)
     buttons = InlineKeyboardMarkup(list(keyboard))
-
-    img = wget.download(thumbnail)
+    
+    img = wget.download(thumbnail, out=False)
     im = Image.open(img).convert("RGB")
     output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
-    if not os.path.isdir(output_directory):
-        os.makedirs(output_directory)
     thumb_image_path = f"{output_directory}.jpg"
     im.save(thumb_image_path,"jpeg")
-    await message.reply_photo(thumb_image_path, caption=title+'\n\n❔ Iltimos, fayl turini tanlang: 👇', reply_markup=buttons)
+    await message.reply_photo(thumb_image_path, caption=f"📹 {title}\n\n{file_sizes}\n\n❔ Iltimos, fayl turini tanlang: 👇", reply_markup=buttons)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# buttons = InlineKeyboardMarkup(list(create_audio_button(audioList)))
-    # buttons = InlineKeyboardMarkup(list(create_video_button(videoList)))
-
-    # img = wget.download(thumbnail)
-    # im = Image.open(img).convert("RGB")
-    # output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
-    # print(output_directory)
-    # if not os.path.isdir(output_directory):
-    #     os.makedirs(output_directory)
-    # thumb_image_path = f"{output_directory}.jpg"
-    # im.save(thumb_image_path,"jpeg")
-    # await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
+@Client.on_message()
+async def _get_insta(c, message: Message):
+    await message.reply
--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -1,48 +1,20 @@
 import asyncio
 import os
 
-from pyrogram import (Client,
-                      InlineKeyboardButton,
-                      InlineKeyboardMarkup,
-                      ContinuePropagation,
-                      InputMediaDocument,
-                      InputMediaVideo,
-                      InputMediaAudio)
+from pyrogram import (Client, ContinuePropagation, filters)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaAudio, InputMediaVideo
 
 from helper.ffmfunc import duration
 from helper.ytdlfunc import downloadvideocli, downloadaudiocli
-from PIL import Image
+from PIL.Image import open
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-
-@Client.on_callback_query()
-async def catch_youtube_fmtid(c, m):
-    cb_data = m.data
-    if cb_data.startswith("ytdata||"):
-        yturl = cb_data.split("||")[-1]
-        format_id = cb_data.split("||")[-2]
-        media_type = cb_data.split("||")[-3].strip()
-        print(media_type)
-        if media_type == 'audio':
-            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "Audio", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
-                                                                                                    callback_data=f"docaudio||{format_id}||{yturl}")]])
-        else:
-            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "Video", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
-                                                                                                    callback_data=f"docvideo||{format_id}||{yturl}")]])
-
-        await m.edit_message_reply_markup(buttons)
-
-    else:
-        raise ContinuePropagation
 
 
 @Client.on_callback_query()
 async def catch_youtube_dldata(c, q):
     cb_data = q.data.strip()
-    #print(q.message.chat.id)
-    # Callback Data Check
+
     yturl = cb_data.split("||")[-1]
     format_id = cb_data.split("||")[-2]
     thumb_image_path = "downloads" + \
@@ -53,19 +25,19 @@ async def catch_youtube_dldata(c, q):
 
     if os.path.exists(thumb_image_path):
         metadata = extractMetadata(createParser(thumb_image_path))
-        #print(metadata)
+
         if metadata.has("width"):
             width = metadata.get("width")
         if metadata.has("height"):
             height = metadata.get("height")
-        img = Image.open(thumb_image_path)
-        if cb_data.startswith(("audio", "docaudio", "docvideo")):
+        img = open(thumb_image_path)
+        if cb_data.startswith("audio"):
             img.resize((320, height))
         else:
             img.resize((90, height))
         img.save(thumb_image_path, "JPEG")
-     #   print(thumb_image_path)
-    if not cb_data.startswith(("video", "audio", "docaudio", "docvideo")):
+
+    if not cb_data.startswith(("video", "audio")):
         print("no data found")
         raise ContinuePropagation
 
@@ -124,22 +96,6 @@ async def catch_youtube_dldata(c, q):
             supports_streaming=True
         )
 
-    if cb_data.startswith("docaudio"):
-        filename = await downloadaudiocli(audio_command)
-        med = InputMediaDocument(
-            media=filename,
-            thumb=thumb_image_path,
-            caption=os.path.basename(filename),
-        )
-
-    if cb_data.startswith("docvideo"):
-        filename = await downloadvideocli(video_command)
-        dur = round(duration(filename))
-        med = InputMediaDocument(
-            media=filename,
-            thumb=thumb_image_path,
-            caption=os.path.basename(filename),
-        )
     if med:
         loop.create_task(send_file(c, q, med, filename))
     else:
@@ -151,7 +107,7 @@ async def send_file(c, q, med, filename):
     try:
         await q.edit_message_reply_markup(
             InlineKeyboardMarkup([[InlineKeyboardButton("Uploading...", callback_data="down")]]))
-        await c.send_chat_action(chat_id=q.message.chat.id, action="upload_document")
+        await c.send_chat_action(chat_id=q.message.chat.id, action="upload_video")
         # this one is not working
         await q.edit_message_media(media=med)
     except Exception as e:
@@ -160,6 +116,5 @@ async def send_file(c, q, med, filename):
     finally:
         try:
             os.remove(filename)
-            os.remove(thumb_image_path)
         except:
             pass
